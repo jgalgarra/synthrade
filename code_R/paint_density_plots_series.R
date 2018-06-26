71d6a6489bf9e05f29ce8d948dd64670b2ee479b
jgalgarra <jgalgarra@coit.es>
2018-06-26 19:35:16 +0200

    Ladel fixed in density plots

--- code_R/paint_density_plots_series.R
+++ code_R/paint_density_plots_series.R
@@ -22,7 +22,7 @@ PaintDensPlot <- function(datos,titletext,xlabel)
 {
   p <- ggplot() + geom_density(aes(x= cuenta, color = collection, fill = collection),  alpha = .1,
                                data=datos, position = "identity", adjust=1.5)+ 
-    xlab(xlabel)+ylab("Count\n")+
+    xlab(xlabel)+ylab("Density\n")+
     ggtitle(titletext)+ scale_x_log10()+
     scale_fill_manual(values=c("blue","white","red"))+
     scale_color_manual(values=c("blue","grey","red"))+
@@ -48,7 +48,7 @@ PaintBoxPlot <- function(datos,titletext,xlabel)
 {
   p <- ggplot() + geom_boxplot(aes(x=as.factor(collection),y= cuenta, color = collection, fill = collection),  alpha = .1,
                                data=datos, position = "identity")+ 
-    xlab(xlabel)+ylab("Count\n")+
+    xlab(xlabel)+ylab("Density\n")+
     ggtitle(titletext)+ scale_y_log10()+
     scale_fill_manual(values=c("blue","white","red"))+
     scale_color_manual(values=c("blue","grey","red"))+
